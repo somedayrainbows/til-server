b8d2c9071174a65d008a9d1cacae4782390869ed
Amy Bertken <amy.bertken@cognizant.com>
2019-10-21 17:19:19 -0600

    add list users functionality

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -2,7 +2,7 @@ const { User } = require('../models');
 
 module.exports = {
 
-    async create(req, res) {
+    async create(req, res){
         const { firstName, lastName, email, admin } = req.body;
         try { 
            const user = await User.create({
@@ -12,8 +12,17 @@ module.exports = {
                 admin 
             });
             res.status(201).send(user);
-        } catch(error){           
+        } catch(error) {           
             res.status(400).send(error)
+        }
+    },
+
+    async list(req, res) {
+        try {
+            const users = await User.findAll();
+            res.status(200).send(users);
+        } catch (error) {
+            res.status(400).send(error);
         }
     }
 }
--- server/routes/index.js
+++ server/routes/index.js
@@ -7,4 +7,6 @@ module.exports = (app) => {
     }));
 
     app.post('/api/users', usersController.create);
+
+    app.get('/api/users', usersController.list);
 }
